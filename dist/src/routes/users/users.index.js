4a9abf4e0d84787502235a48c676b0b72387246d
akash-lb <akash@lightbeam.ai>
2024-11-13 17:51:42 +0530

    fix: remove dist

--- dist/src/routes/users/users.index.js
+++
@@ -1,4 +0,0 @@
-import { createRouter } from "../../lib/create-app.js";
-import * as handlers from "./users.handlers.js";
-import * as routes from "./users.routes.js";
-export const UserRouter = createRouter().openapi(routes.getUser, handlers.getUserById).openapi(routes.registerUser, handlers.registerUser).openapi(routes.loginUser, handlers.loginUser);
--- dist/src/utils/jwt.js
+++
@@ -1,9 +0,0 @@
-import { sign, verify } from "hono/jwt";
-import env from "../env.js";
-export async function generateToken(userId) {
-    const token = await sign({ userId }, env.JWT_SECRET);
-    return token;
-}
-export async function verifyToken(token) {
-    return await verify(token, env.JWT_SECRET);
-}
